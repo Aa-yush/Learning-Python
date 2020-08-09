e209ea3f1902dceb135e74941e0da9304fe712a8
Ayush _Gupta <31875557+Aa-yush@users.noreply.github.com>
2020-08-09 21:03:39 +0530

    Add files via upload

--- AverageCalc.py
+++ AverageCalc.py
@@ -1,4 +1,4 @@
 print("Calculating average of 4 and 9")
-number1 = float(input("Enter First Number : "))
-number2 = float(input("Enter First Number : "))
+number1 = 4
+number2 = 9
 print("Average is : ",(number1+number2)/2)
