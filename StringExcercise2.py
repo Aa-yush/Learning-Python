39af402d0163452d6bf9192ea7596f11b8e39b0e
Ayush _Gupta <31875557+Aa-yush@users.noreply.github.com>
2020-08-09 21:05:20 +0530

    Add files via upload

---
+++ StringExcercise2.py
@@ -0,0 +1,7 @@
+print("Product Number : 037-00901-00027")
+pn = "037-00901-00027";
+print("Country code: ",pn[0:3])
+print("Product code: ",pn[4:9])
+print("Batch code: ",pn[-5:])
+
+
---
+++ StringExercise.py
@@ -0,0 +1,4 @@
+fName = input("Enter First Name : ")
+mName = input("Enter Middle Name : ")
+lName = input("Enter Last Name : ")
+print('"Your initial are '+fName+" "+mName+" "+lName+'"')
